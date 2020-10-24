d71902b074d83c8e59b06ed0a8ebb1b71dd4e4ff
Andreas Weisethaunet <weisethaunet@gmail.com>
2020-10-24 19:17:33 +0200

    Improve healtmonitor bat file
    The healthmonitor.bat file now handles server-Path with spaces
    Have also added variables to define HOST,PATH,USER and PASSWORDy

--- healthmonitor/healthmonitor.bat
+++ healthmonitor/healthmonitor.bat
@@ -1,35 +1,49 @@
 @ECHO off
+
+set SERVER_HOST="HOST"
+set SERVER_CLIENT_FOLDER="FULL URL TO CLIENT FOLDER"
+set USER="SERVER USER"
+set PASSWORD="SERVER PASSWORD"
+
+taskkill /F /FI "WindowTitle eq client" /T
+start "client" java -jar client.jar
+
 :while
-    net use \\FENRIZ-PC /user:INSERT-USERNAME INSERT-PASSWORD
+    net use %SERVER_HOST% /user:%USER% %PASSWORD%
     echo checking for config updates...
+
+    copy %SERVER_CLIENT_FOLDER%\config.txt configTemp.txt
+
     for /f "tokens=1,2 delims==" %%G in (config.txt) do set %%G=%%H
     set localVersion=%version%
     set localRestart=%restart%
 
-    for /f "tokens=1,2 delims==" %%G in (\\FENRIZ-PC\mp32\config\config.txt) do set %%G=%%H
+    echo localVersion %localVersion%
+    echo localRestart %localRestart%
+
+    echo checking remote config
+    for /f "tokens=1,2 delims==" %%G in (configTemp.txt) do set %%G=%%H
     set remoteVersion=%version%
     set remoteRestart=%restart%
 
-    echo localVersion %localVersion%
-    echo localRestart %localRestart%
     echo remoteVersion %remoteVersion%
     echo remoteRestart %remoteRestart%
 
-    IF %localRestart% LSS %remoteRestart% (
+    IF %localVersion% LSS %remoteVersion% (
         echo restart is scheduled, nr %remoteRestart%
-        copy \\FENRIZ-PC\mp32\config\config.txt config.txt
+        copy configTemp.txt config.txt
+        copy %SERVER_CLIENT_FOLDER%\client.jar client.jar
         taskkill /F /FI "WindowTitle eq client" /T
         start "client" java -jar client.jar
         echo restarting client
     )
 
-    IF %localVersion% LSS %remoteVersion% (
+    IF %localRestart% LSS %remoteRestart% (
         echo restart is scheduled, nr %remoteRestart%
-        copy \\FENRIZ-PC\mp32\config\config.txt config.txt
-        copy \\FENRIZ-PC\mp32\config\client.jar client.jar
+        copy configTemp.txt config.txt
         taskkill /F /FI "WindowTitle eq client" /T
-        start "client" java -jar client.jar
         echo restarting client
+        start "client" java -jar client.jar
     )
     TIMEOUT 10
 goto :while
